f551b184730c3e35a9573ae3eb92139469c0337c
Hexazeta <124427272+Hexazeta@users.noreply.github.com>
2024-05-27 11:30:41 +0700

    e

---
+++ src/complicatedustry/Complicatedustry.java
@@ -0,0 +1,14 @@
+package complicatedustry;
+
+import mindustry.mod.*;
+import complicatedustry.gen.*;
+
+public class Complicatedustry extends Mod{
+    @Override
+    public void loadContent() {
+        EntityRegistry.register();
+
+    }
+
+    public static class EntityRegistry { public static void register(){}}
+}
